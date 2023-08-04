43bb4b09112847b50878b1d7aaced2e32ecaf4f0
Santiago9j <santitorresaguirre@gmail.com>
2023-08-04 11:30:18 -0500

    exepciones personalizadas

---
+++ 12exepcionesPersonalizadas.py
@@ -0,0 +1,9 @@
+#Personalizando errores
+class Err(Exception):
+    def __init__(self,valor):
+        print(f'Se causo un error por:{valor}')
+
+try:
+    raise Err(2)
+except Err:
+    print('Error escrito')
