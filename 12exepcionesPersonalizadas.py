1a65a227f73b6bdb0d54d2c8ad73ae3e6048ad3b
Santiago9j <santitorresaguirre@gmail.com>
2023-08-04 11:31:51 -0500

    documentacion

--- 12exepcionesPersonalizadas.py
+++ 12exepcionesPersonalizadas.py
@@ -1,7 +1,9 @@
 #Personalizando errores
+#Estructura
+#Se crea una clase que herede de Exception
 class Err(Exception):
-    def __init__(self,valor):
-        print(f'Se causo un error por:{valor}')
+    def __init__(self,valor): 
+        print(f'Se causo un error por:{valor}')  
 
 try:
     raise Err(2)
