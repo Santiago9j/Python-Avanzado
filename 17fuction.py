40ecd5131af203256db6fda7997b9acac7e4b378
Santiago9j <santitorresaguirre@gmail.com>
2023-08-09 15:25:35 -0500

    funciones

--- 17fuction.py
+++ 17fuction.py
@@ -6,3 +6,18 @@ lista = ['LAURa','JhoANA','si']
 #Map,primero se le pasa la funcion, luego la lista que va a iterar en dicha funcion
 
 print(list(map(lower,lista)))
+
+
+def saludar(idioma):
+    def saludar_es():
+        print('Hola')
+    def saludar_en():
+        print('Hello')
+    func_idioma = {
+        'es':saludar_es,
+        'en':saludar_en
+    }
+    return func_idioma[idioma]
+
+el_saludo = saludar('es')
+el_saludo()
