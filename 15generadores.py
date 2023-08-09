1394d35183d180c195a0f5ee057a4ffb85f1bbda
Santiago9j <santitorresaguirre@gmail.com>
2023-08-09 15:04:21 -0500

    Generadores

--- 15generadores.py
+++ 15generadores.py
@@ -1,6 +1,7 @@
 def generarPares(limites):
     num = 1
     while num<=limites:
+        # Yield permite generar valores uno a uno en vez de todas al mismo tiempo
         yield num*2
         num+=1
     
@@ -8,3 +9,12 @@ devolverPares = generarPares(3)
 
 print(next(devolverPares)) # 2
 print(next(devolverPares)) # 4
+
+def generaPares(n):
+    num = 1 
+    while num <= n:
+        yield num * 2
+        num+=1 
+n = 10
+for num in generaPares(n):
+    print(num)
