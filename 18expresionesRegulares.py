912633f5f8ca72032d545588d3072dbc7937d5c8
Santiago9j <santitorresaguirre@gmail.com>
2023-08-22 10:23:02 -0500

    patrones

--- 18expresionesRegulares.py
+++ 18expresionesRegulares.py
@@ -3,3 +3,7 @@ import re
 print(re.search(r'j',"hoanja")) #<re.Match object; span=(4, 5), match='j'>
 
 print(re.search(r'\d\d', "a12b")) #<re.Match object; span=(1, 3), match='12'>
+
+
+if (re.search('\Aa[0-9].*(end|fin)$','a4 es una marca de fin')): 
+    print('Se cumple el patron')
