28a84b0808c1adeff614384dbeb45762e622252c
Santiago Torres Aguirre <santitorresaguirre@gmail.com>
2023-09-12 08:17:03 -0500

    Create ejemplo.java

---
+++ ejemplo.java
@@ -0,0 +1,113 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.holamundo;
+
+import java.util.Scanner;
+
+
+
+
+
+/**
+ *
+ * @author Santiago Torres
+ */
+public class App {
+
+    
+    public static void main(String[] args) {
+        //Información
+        /*
+        Scanner lectura = new Scanner(System.in);
+        System.out.println("Ingrese su nombre: ");
+        String nombre = lectura.next();
+        System.out.println("Ingrese su edad: ");
+        int edad = lectura.nextInt();
+        System.out.println("Ingrese su ciudad");
+        String ciudad = lectura.next();
+        System.out.println("Ingrese su estatura.");
+        double estatura = lectura.nextDouble();
+        
+        
+        System.out.println("La información registrada es: \n"+
+                "Nombre = "+nombre+"\n"+
+                "Edad = "+edad+"\n"+
+                "Ciudad = "+ciudad+"\n"+
+                "Estatura = "+estatura
+        );*/
+        
+        
+        
+        //System.out.println(5 / 0);
+        
+        /*
+        try {
+            System.out.println(5 / 0);
+        } catch(ArithmeticException e){
+            System.out.println("Lo siento, no puedo dividir por cero.");
+        }
+        */
+        
+        
+        /*
+        
+        Scanner lectura = new Scanner(System.in);
+        
+        
+        String nombre;
+        do {
+            System.out.print("Ingrese su nombre: ");
+            nombre = lectura.next().trim(); // Elimina espacios en blanco al inicio y al final
+            if (nombre.isEmpty()) {
+                System.out.println("Error: El nombre no puede estar vacío.");
+            }
+        } while (nombre.isEmpty());
+        
+        
+        int edad;
+        do {
+            System.out.print("Ingrese su edad: ");
+            while (!lectura.hasNextInt()) {
+                System.out.println("Error: Ingrese una edad válida.");
+                System.out.print("Ingrese su edad: ");
+                lectura.next(); // Limpia el búfer del Scanner
+            }
+            edad = lectura.nextInt();
+            if (edad <= 0) {
+                System.out.println("Error: La edad debe ser un número positivo.");
+            }
+        } while (edad <= 0);
+        
+        
+        
+        String ciudad;
+        do {
+            System.out.print("Ingrese su Ciudad: ");
+            ciudad = lectura.next().trim(); // Elimina espacios en blanco al inicio y al final
+            if (ciudad.isEmpty()) {
+                System.out.println("Error: La ciudad no puede estar vacío.");
+            }
+        } while (ciudad.isEmpty());
+        
+        
+        
+        
+        System.out.println("Nombre = "+nombre+"\n"+"Edad = "+edad+"\n"+"Ciudad = "+ciudad);
+        
+        
+        
+        
+        
+        lectura.close();*/
+        
+        
+        
+        
+        
+        
+        
+    }
+}
